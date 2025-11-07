4845fe600dc9cbaaa5885e057e3dac5181fc8357
kernel <charlintoxic@gmail.com>
2025-11-07 19:10:28 -0300

    Update perplexity.py

--- perplexity.py
+++ perplexity.py
@@ -1,4 +1,4 @@
-#EXTINF:-1 tvg-id="rootaws" group-title="" tvg-name=" " tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpHfrAigET0ke6Gk0LhBrwV2UrBlsVFJfWvfjjNkkCiu4C2sljtbiFd0JD&s=10",
-http://provedorduo.xyz/415572/579388.m3u8
+#EXTINF:-1 tvg-id="rootaws" group-title="root" tvg-name="Perplexity" tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpHfrAigET0ke6Gk0LhBrwV2UrBlsVFJfWvfjjNkkCiu4C2sljtbiFd0JD&s=10",
+http://dnssp.site/live/josue206/cypioalt.m3u8
 
 #perplexitycode
