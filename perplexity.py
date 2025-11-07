e9d6edf72f3c48978da1ab4de7f9305be75be3b5
kernel <charlintoxic@gmail.com>
2025-11-07 19:11:40 -0300

    Update perplexity.py

--- perplexity.py
+++ perplexity.py
@@ -1,4 +1,4 @@
 #EXTINF:-1 tvg-id="rootaws" group-title="root" tvg-name="Perplexity" tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpHfrAigET0ke6Gk0LhBrwV2UrBlsVFJfWvfjjNkkCiu4C2sljtbiFd0JD&s=10",
-http://dnssp.site/live/josue206/cypioalt.m3u8
+http://dnssp.site:80/get.php?username=josue206&password=cypioalt&type=m3u_plus
 
 #perplexitycode
