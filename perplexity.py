a26fe4cb6471467351b42e7b9f36e81a65821aff
kernel <charlintoxic@gmail.com>
2025-11-07 18:58:43 -0300

    Update perplexity.py

--- perplexity.py
+++ perplexity.py
@@ -1,4 +1,4 @@
-#perplexitycode
-
 #EXTINF:-1 tvg-id="rootaws" group-title="" tvg-name=" " tvg-logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpHfrAigET0ke6Gk0LhBrwV2UrBlsVFJfWvfjjNkkCiu4C2sljtbiFd0JD&s=10",
-http://assistenciatecnica.top:80/797349698/539301407.m3u8
+http://assistenciatecnica.top/797349698/539301407.m3u8
+
+#perplexitycode
